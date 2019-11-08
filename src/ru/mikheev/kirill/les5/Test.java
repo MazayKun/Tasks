83957685dcf6f00a429a1dd071ed4dc5a0073b9c
MazayKun <kir99916@gmail.com>
2019-11-08 23:07:27 +0300

    Add class animal for les5.task

---
+++ src/ru/mikheev/kirill/les5/Test.java
@@ -0,0 +1,12 @@
+package ru.mikheev.kirill.les5;
+
+/**
+ * @author Kirill Mikheev
+ * @version 1.0
+ */
+
+public class Test {
+    public static void main(String[] argc) {
+        System.out.println("Puff");
+    }
+}
---
+++ src/ru/mikheev/kirill/les5/task/AnimalDataBank.java
@@ -0,0 +1,9 @@
+package ru.mikheev.kirill.les5.task;
+
+/**
+ * @author Kirill Mikheev
+ * @version 1.0
+ */
+
+public class AnimalDataBank {
+}
