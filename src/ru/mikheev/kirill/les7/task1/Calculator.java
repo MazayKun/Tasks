223a48b7bfa67bda10394e29dc45a9a0e05a8cee
MazayKun <kir99916@gmail.com>
2019-11-14 18:59:07 +0300

    Добавил доки, провел базовый рефакторинг Как мне кажется, от кода еще попахивает, но хоть что-то показать нужно

--- src/ru/mikheev/kirill/les7/task1/Calculator.java
+++ src/ru/mikheev/kirill/les7/task1/Calculator.java
@@ -4,20 +4,33 @@ import java.math.BigInteger;
 import java.util.concurrent.Callable;
 
 /**
+ * Класс, реализующий интерфейс Callable
+ * Считает и возвращает произведение чисел на определенном промежутке
  * @author Kirill Mikheev
  * @version 1.0
  */
 
 public class Calculator implements Callable<BigInteger> {
 
+    /** Начало и конец промежутка, на котором нужно посчитать произведение */
     private Integer start;
     private Integer end;
 
+
+    /**
+     * Конструктор получающий на вход нужный нам промежуток
+     * @param start начало промежутка
+     * @param end конец промежутка
+     */
     public Calculator(Integer start, Integer end) {
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Функция считает произведение чисел на промежутке [start, end] и возвращает объект класса BigInteger
+     * @return BigInteger - произвдение всех чисел на промежутке
+     */
     @Override
     public BigInteger call() {
         BigInteger answ = new BigInteger(start.toString());
--- src/ru/mikheev/kirill/les7/task1/ThreadPull.java
+++ src/ru/mikheev/kirill/les7/task1/ThreadPull.java
@@ -6,20 +6,42 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 /**
+ * Объект, который позволяет считать факториалы чисел в массиве
  * @author Kirill Mikheev
  * @version 1.0
  */
 
 public class ThreadPull {
 
+    /**
+     * Метод получает на вход массив чисел и количетсво потоков
+     * Считает для каждого элемента в массиве его факторил, распараллеливая задачу на заданное количество потоков
+     * @param array массив чисел на подсчет факториалов
+     * @param threadCount желаемое количетсво потоков
+     * @return массив объектов BigInteger, в котором храняться факториалы
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     public BigInteger[] calculateFactorials(Integer[] array, int threadCount) throws ExecutionException, InterruptedException {
+
         BigInteger[] answ = new BigInteger[array.length];
+
         for (int i = 0; i < array.length; i++){
             answ[i] = calculateFactorial(array[i], threadCount);
         }
+
         return answ;
     }
 
+    /**
+     * Приватный метод, который распараллеливает вычисление факториала заданного числа на заданное количество потоков
+     * Если число меньше количетсва потоков, то все вычисления производятся в одном потоке
+     * @param number само число
+     * @param threadCount количество потоков
+     * @return объект BigInteger - ответ
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
     private BigInteger calculateFactorial(Integer number, int threadCount) throws ExecutionException, InterruptedException {
         if(number == 0) {
             return new BigInteger("1");
