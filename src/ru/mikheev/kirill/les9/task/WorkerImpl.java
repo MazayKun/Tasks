5039b486adbdb453430d716e9f05eb1eeb96c258
MazayKun <kir99916@gmail.com>
2019-11-24 01:16:26 +0300

    Забыл догрузить файл для тестирования

---
+++ src/ru/mikheev/kirill/les9/task/WorkerImpl.java
@@ -0,0 +1,10 @@
+package ru.mikheev.kirill.les9.task;
+
+/**
+ * В итоге это вручную созданный класс, который выполняет, что мне надо
+ */
+public class WorkerImpl implements Worker{
+	public void doWork(){
+        System.out.println("work");
+	}
+}
